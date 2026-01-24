700d63dddca0748188a2e372f07c4d835caa7444
Amrita Thapa <AmritaThapa2626@gmail.com>
2026-01-24 16:00:09 -0600

    was not updating count for different matches before

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -4,22 +4,22 @@
 
 int majorityElement(std::vector<int> &arr) {
     std::sort(arr.begin(), arr.end());
-    if(arr.size() == 1) {
+    int n = arr.size();
+    if(n == 1) {
         return arr[0];
     }
     int count = 1;
-    int value = 0;
-    for(int i = 0; i < arr.size() - 2; i ++) {
-        if (arr[i] == arr[i + 1]) {
+    int value = arr[n/2];
+    for(int i = 0; i < n - 2; i ++) {
+        if (arr[i] == value) {
             count++;
-            value = arr[i];
         }
     }
     return count > arr.size() / 2 ? value : -1;
 }
 
 int main() {
-    std::vector<int> arr = {2, 13};
+    std::vector<int> arr = {1, 1, 2, 1, 3, 5, 1};
     std::cout << majorityElement(arr);
     return 0;
 }
